fded23dd2c77af0b52175302cf17dfc2025a9cb3
a5566baga <a5566baga@126.com>
2017-03-05 15:38:25 +0800

    初始化

---
+++ FItBodyUser/FItBodyUser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FItBodyUser
+//
+//  Created by ben on 17/3/5.
+//  Copyright © 2017年 张增强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
