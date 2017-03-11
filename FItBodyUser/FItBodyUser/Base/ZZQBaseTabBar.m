8a64d7ec7cc374a38e749372aedc67f5f0666a92
a5566baga <a5566baga@126.com>
2017-03-11 21:42:51 +0800

    初始化完成

---
+++ FItBodyUser/FItBodyUser/Base/ZZQBaseTabBar.m
@@ -0,0 +1,21 @@
+//
+//  ZZQBaseTabBar.m
+//  FItBodyUser
+//
+//  Created by ben on 17/3/11.
+//  Copyright © 2017年 张增强. All rights reserved.
+//
+
+#import "ZZQBaseTabBar.h"
+
+@implementation ZZQBaseTabBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
